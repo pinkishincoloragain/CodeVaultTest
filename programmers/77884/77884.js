c8f8947100ddb1ca496114a712d2521e3c7759d5
pinkishincoloragain <smb1103@gmail.com>
2023-10-11 17:36:34 +0900

    [programmers] 77884 (Time: 0.37ms, Memory: 33.7MB) - by Code-Vault

--- programmers/77884/77884.js
+++ programmers/77884/77884.js
@@ -1,13 +1,5 @@
 function solution(left, right) {
-    const getDivsNum = num => {
-        let cnt = 0;
-        for(let i=1; i ** 2 <= num; i+=1){
-            if(num % i === 0) cnt += 2;
-            if(num === i ** 2) cnt -= 1;
-        }
-        return cnt;
-    }
-        
     return Array.from({length: right - left + 1}, (_, idx) => idx + left)
-        .reduce((acc, cur) => getDivsNum(cur) % 2 === 0 ? acc + cur : acc - cur, 0);
+        .reduce((acc, cur) => Math.floor(Math.sqrt(cur)) === Math.sqrt(cur)
+                ? acc - cur : acc + cur, 0);
 }
