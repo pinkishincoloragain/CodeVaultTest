87d34ee9cc57332385f2de8b6ae7270ab46d79b1
pinkishincoloragain <smb1103@gmail.com>
2023-10-19 02:26:12 +0900

    [programmers] 17681 (Time: 0.05ms, Memory: 10.3MB) - by Code-Vault

--- programmers/17681/17681.py
+++ programmers/17681/17681.py
@@ -1,9 +1,16 @@
+# def solution(n, arr1, arr2):
+#     answer = []
+    
+#     for i, j in zip(arr1, arr2):
+#         line = bin(i | j)[2:]
+#         res = ''.join(list(map(lambda x: '#' if x == '1' else ' ', list(str(line).zfill(n)))))
+#         answer.append(res)
+    
+#     return answer
+
 def solution(n, arr1, arr2):
-    answer = []
-    
-    for i, j in zip(arr1, arr2):
-        line = bin(i | j)[2:]
-        res = ''.join(list(map(lambda x: '#' if x == '1' else ' ', list(str(line).zfill(n)))))
-        answer.append(res)
-    
-    return answer
+    # answer = []
+    return [''.join(list(map(lambda x: '#' if x == '1' else ' ', list(str(bin(i | j)[2:]).zfill(n))))) for i, j in zip(arr1, arr2)]
+    #     res = ''.join(list(map(lambda x: '#' if x == '1' else ' ', list(str(bin(i | j)[2:]).zfill(n)))))
+    #     answer.append(res)
+    # return answer
