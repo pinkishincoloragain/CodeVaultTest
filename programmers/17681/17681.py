853ad71c14ff3c933a2af0ab5aa21cf916c46348
pinkishincoloragain <smb1103@gmail.com>
2023-10-19 02:24:23 +0900

    [programmers] 17681 (Time: 0.05ms, Memory: 10.3MB) - by Code-Vault

---
+++ programmers/17681/17681.py
@@ -0,0 +1,9 @@
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for i, j in zip(arr1, arr2):
+        line = bin(i | j)[2:]
+        res = ''.join(list(map(lambda x: '#' if x == '1' else ' ', list(str(line).zfill(n)))))
+        answer.append(res)
+    
+    return answer
